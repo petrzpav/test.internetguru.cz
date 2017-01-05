e5d6447bbd467757260cfbdcee1c8f400ed3f4d3
petrzpav <pavel@petrzela.eu>
2017-01-05 13:40:49 +0100

    travis: fix deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ TO_UPLOAD=()
 
 for file in ${CHANGED_FILES[@]}; do
   [[ "$file" == ".travis.yml" || "$file" == "deploy.sh" ]] && continue
-  curl -k --ftp-ssl --ftp-create-dirs -T "$(basename "$file")" -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/$WEBSITE/"$(dirname "$file")/"
+  curl -k --ftp-ssl --ftp-create-dirs -T "$file" -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/$WEBSITE/"$(dirname "$file")/"
 done
 
 
