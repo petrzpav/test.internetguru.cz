fd18e0521d94701719d647308bd9dc649e304a0b
petrzpav <pavel@petrzela.eu>
2017-01-05 13:21:50 +0100

    travis: fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
-curl -k --ftp-ssl --ftp-create-dirs -T "{$(IFS=,; echo "${CHANGED_FILES[*]}")}" -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/$WEBSITE/
+TO_UPLOAD=()
+
+for file in ${CHANGED_FILES[@]}; do
+  [[ "$file" != ".travis.yml" && "$file" != "deploy.sh" ]] && continue
+  TO_UPLOAD+=("$file")
+done
+
+[[ ${#TO_UPLOAD[@]} -eq 0 ]] \
+  || curl -k --ftp-ssl --ftp-create-dirs -T "{$(IFS=,; echo "${TO_UPLOAD[*]}")}" -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/$WEBSITE/
