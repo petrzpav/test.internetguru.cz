146f74902980cff628e9198059be68774b906a91
petrzpav <pavel@petrzela.eu>
2017-01-05 11:38:19 +0100

    travis: fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-ls -la
-git log --oneline --all --graph --decorate
-
 CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
 curl -k --ftp-ssl --ftp-create-dirs -T "{$(IFS=,; echo ${CHANGED_FILES[@]})}" -u $FTP_USER:$FTP_PASS ftp://31.31.75.247:21/$WEBSITE
