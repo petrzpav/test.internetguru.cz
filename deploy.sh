4f7d56b62ae075c33a3382ce64fb399a988e78a9
root <root@vm14848>
2017-04-13 16:20:16 +0200

    Update 2 items. Added 5 items. Removed 1 items.

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,11 @@ CHANGED_FILES=($(git diff --name-only $TRAVIS_COMMIT_RANGE))
 TO_UPLOAD=()
 
 for file in ${CHANGED_FILES[@]}; do
-  [[ "$file" == ".travis.yml" || "$file" == "deploy.sh" ]] && continue
+  [[ "$file" == ".travis.yml" || "$file" == "deploy.sh" || "$file" == ".gitignore" ]] && continue
+  if [[ ! -f "$file" ]]; then
+    curl -v -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/"$WEBSITE/$file" -Q "DELE $file"
+    continue
+  fi
   curl -k --ftp-ssl --ftp-create-dirs -T "$file" -u "$FTP_USER":"$FTP_PASS" ftp://31.31.75.247:21/$WEBSITE/"$(dirname "$file")/"
 done
-
 
